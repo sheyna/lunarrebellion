65e21be6441c5a29c871d92192cb447e0ea55137
Watkins <watkins@sea-watkins-m3.ds.ad.adp.com>
2018-03-18 21:41:23 -0700

    Got avatar moving on board.

--- public/js/moveAvatar.js
+++ public/js/moveAvatar.js
@@ -1,13 +1,31 @@
-function moveAvatar(startingPosition, queryClick) {
+function moveAvatar(domElement, startingPosition) {
   startingPosition = parseInt(startingPosition);
+  queryClick = domElement.dataset.boxposition;
   queryClick = parseInt(queryClick);
-  if (startingPostion < 24 && queryClick === startingPosition + 6) {
+
+  if (startingPosition < 24 && queryClick === startingPosition + 6) {
       // you can move one down
+      startingPosition = makeMove(queryClick);
+
   } else if (startingPosition > 5 && queryClick === startingPosition - 6) {
       // you can move one up
-  } else if ((startingPostion !== 0 || startingPostion % 6 === 0) && queryClick === startingPosition + 1) {
-     // you can move to right
-  } else if ((startingPostion !== 5 || startingPostion !== 11 || startingPostion !== 17 || startingPostion !== 23 || startingPostion !== 29) && queryClick === startingPosition - 1) {
+      startingPosition = makeMove(queryClick)
+
+  } else if (startingPosition !== 0 && startingPosition % 6 !== 0 && queryClick === startingPosition - 1) {
      // you can move to left
+     startingPosition = makeMove(queryClick);
+
+  } else if ((startingPosition + 1) % 6 !== 0 && queryClick === startingPosition + 1) {
+     // you can move to right
+     startingPosition = makeMove(queryClick);
   }
+  return startingPosition
+}
+
+function makeMove(queryClick) {
+  // move avatar:
+  $("#playerAvatar").prependTo(".boardTile[data-boxposition='" + queryClick + "']");
+  // update current position variable:
+  startingPosition = queryClick;
+  return startingPosition
 }
